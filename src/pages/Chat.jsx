ddbbddb540bb63b02049a16dc272e769298bce31
Edit Selmeczi <selmeczi.e@gmail.com>
2023-07-26 18:30:20 +0200

    add home button to sub pages

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,9 +1,17 @@
-function Chat(){
-  return(
+import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+
+
+function Chat() {
+  return (
     <div>
+      <Link to={"/home"}>
+        <HomeIcon />
+        Home
+      </Link>
       <h1>Chat</h1>
     </div>
-  )
+  );
 }
 
 export default Chat;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,16 @@
-function Login(){
-  return(
+import HomeIcon from "@mui/icons-material/Home";
+import { Link } from "react-router-dom";
+
+function Login() {
+  return (
     <div>
+      <Link to={"/home"}>
+        <HomeIcon />
+        Home
+      </Link>
       <h1>Login</h1>
     </div>
-  )
+  );
 }
 
 export default Login;
