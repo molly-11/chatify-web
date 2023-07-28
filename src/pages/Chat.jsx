29ab9bc8ca5409090f709387dfa28c2b19aabe36
Edit Selmeczi <selmeczi.e@gmail.com>
2023-07-28 08:29:54 +0200

    add chat rooms

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -3,10 +3,28 @@ import HomeIcon from "@mui/icons-material/Home";
 import { useAuth } from "../context/AuthProvider";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import store from "../firestore";
+import { collection, query } from "firebase/firestore";
+import { Button, Grid, Paper } from "@mui/material";
+
+const roomsRef = collection(store, "chat rooms").withConverter({
+  fromFirestore(snap) {
+    return {
+      id: snap.id,
+      ...snap.data(),
+    };
+  },
+  toFirestore(doc) {
+    return doc;
+  },
+});
+const roomsQuery = query(roomsRef);
 
 function Chat() {
   const user = useAuth();
   const navigate = useNavigate();
+  const [rooms, loading] = useCollectionData(roomsQuery);
 
   useEffect(() => {
     if (!user) {
@@ -19,13 +37,27 @@ function Chat() {
   }
 
   return (
-    <div>
+    <>
       <Link to={"/"}>
         <HomeIcon />
         Home
       </Link>
       <h1>Chat</h1>
-    </div>
+      <Grid container spacing={2} sx={{ p: 2 }}>
+      {loading
+        ? "Chat rooms loading"
+        : rooms.map((room) => (
+            <Grid item xs={12} md={6} lg={4} key={room.id}>
+              <Paper sx={{ padding: "10px" }}>
+                <h1>{room.name}</h1>
+                <Link to={`/chat/${room.id}`}>
+                  <Button variant="contained">Go to this room</Button>
+                </Link>
+              </Paper>
+            </Grid>
+          ))}
+    </Grid>
+    </>
   );
 }
 
